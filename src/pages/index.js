d8dd8a24e7e3c25ab8cc12e234c66bfba97e7182
Julian Chan <julian@Julians-MacBook-Pro.local>
2017-10-04 23:56:51 +1100

    fix attributes

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,6 @@ import Link from "gatsby-link"
 export default () => 
     <div style={{ color: 'tomato' }}>
         <h1>Do you like my random picture</h1>
-        <img src="https://placeimg.com/400/200/any" alt="LoremPixel" />
+        <img src="https://placeimg.com/400/200/any" alt="Place IMG" width="400" height="200" />
         <Link to="/page-2">Page 2</Link>
     </div>
