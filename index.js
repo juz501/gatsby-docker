49dd1a87309e12477866d5607c545c7596b2912a
Julian Chan <julian@Julians-MacBook-Pro.local>
2017-10-04 23:27:27 +1100

    ssl and compression

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 var express = require('express'); 
+var compression = require('compression');
+var sslRedirect = require('heroku-ssl-redirect');
+
 var app = express();
+app.use(sslRedirect());
+
+function shouldCompress(req, res) {
+    if (req.headers['x-no-compression']) {
+        return false;
+    }
+    return compression.filter(req, res);
+}
+app.use(compression({ filter: shouldCompress }));
+
 app.use(express.static(__dirname + '/public'));
 
 app.listen(process.env.PORT || 3000);
